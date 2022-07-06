b83605054888f6ee7c8558ae9ea6d8a531beb03e
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2022-07-06 22:29:54 +0200

    Mandatory iterations - done

--- app.js
+++ app.js
@@ -22,4 +22,24 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+    .then(beersFromApi => {
+      const data = { 
+        beers: beersFromApi
+      }
+      res.render('beers', data)
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI.getRandom()
+    .then(randomBeer => {
+      res.render('randomBeer', randomBeer[0])
+    })
+    .catch(error => console.log(error))
+});
+
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
